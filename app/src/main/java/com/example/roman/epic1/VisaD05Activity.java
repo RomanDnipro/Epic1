6ac8ce22a63e5e4a2df2a30f158dd4a20026147c
Roman Bogdanov <bogdanovroman1221@gmail.com>
2017-05-29 04:04:43 +0300

    activity_visa_d05

---
+++ app/src/main/java/com/example/roman/epic1/VisaD05Activity.java
@@ -0,0 +1,13 @@
+package com.example.roman.epic1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VisaD05Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visa_d05);
+    }
+}
